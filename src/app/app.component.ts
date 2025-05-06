3f8087e419d8ca0d77d3ae96e8e4500904d8ac4e
matelat-daw <cesarmatelat@gmail.com>
2025-05-06 14:31:33 +0100

    Update 6-5-2025 14:31

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -65,6 +65,15 @@ export class AppComponent {
     image: new FormControl<File | null>(null)
   });
 
+  onFileSelected(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files.length > 0) {
+      const file = input.files[0];
+      this.registerForm.patchValue({ image: file });
+      this.registerForm.get('image')?.updateValueAndValidity(); // Asegúrate de que el valor sea válido
+    }
+  }
+
   onRegisterFormSubmit() {
     const userData = {
       Name: this.registerForm.value.name,
