1c3967c147a921a3128074ce380c664933a34dec
Orion68 <orions68@gmail.com>
2025-05-30 12:34:01 +0100

    Update Clear Project

--- src/app/models/register.ts
+++
@@ -1,8 +0,0 @@
-export interface Register {
-    nick: string,
-    name: string,
-    surname1: string,
-    email: string,
-    password: string,
-    password2: string,
-}
--- src/app/services/environments/environments.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-    production: false,
-    googleClientId: '1071917637623-020l5qbcihpj4u7tdv411cov4cfh530c.apps.googleusercontent.com'
-  };
